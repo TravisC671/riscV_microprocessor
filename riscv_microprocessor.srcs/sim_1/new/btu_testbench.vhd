c24b2ba9d6184f0e9696cefdbce2a6e5077d1680
travisc671 <travisc088@gmail.com>
2025-09-05 14:10:54 -0600

    finsihed testing register_file

---
+++ riscv_microprocessor.srcs/sim_1/new/btu_testbench.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/05/2025 02:09:59 PM
+-- Design Name: 
+-- Module Name: btu_testbench - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity btu_testbench is
+--  Port ( );
+end btu_testbench;
+
+architecture Behavioral of btu_testbench is
+
+begin
+
+
+end Behavioral;
