bb3b5f517ac89471aefcd28f4c15f31ec3125e87
travisc671 <travisc088@gmail.com>
2025-12-03 13:49:47 -0700

    adding axi lite tests

--- riscv_microprocessor.srcs/sources_1/new/sys_counter.vhd
+++ riscv_microprocessor.srcs/sources_1/new/sys_counter.vhd
@@ -32,10 +32,41 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sys_counter is
-    Generic (Counters : integer := 8);
-    Port ( clk, res, en : in STD_LOGIC;
-           interrupt : out STD_LOGIC;
-           dout: out std_logic_vector(63 downto 0));
+    Generic (Counters           : integer := 8;
+    		 C_S_AXI_DATA_WIDTH : integer := 32;
+             C_S_AXI_ADDR_WIDTH : integer := 32);
+    Port ( 
+		clk, res, en : in STD_LOGIC;
+        interrupt : out STD_LOGIC;
+        dout: out std_logic_vector(63 downto 0);
+        -- Write Address Channel (AW) - Master to Slave --------------------------
+        S_AXI_AWADDR          : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);              -- Write Address
+        S_AXI_AWPROT          : in  std_logic_vector(2 downto 0);                                 -- Protection Type (not typically used in Lite, but required)
+        S_AXI_AWVALID         : in  std_logic;                                                    -- Write Address Valid (Master asserts)
+        S_AXI_AWREADY         : out std_logic;                                                    -- Write Address Ready (Slave asserts)
+
+        -- Write Data Channel (W) - Master to Slave ------------------------------
+        S_AXI_WDATA           : in  std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);              -- Write Data
+        S_AXI_WSTRB           : in  std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);          -- Write Strobe (Byte enables)
+        S_AXI_WVALID          : in  std_logic;                                                    -- Write Data Valid (Master asserts)
+        S_AXI_WREADY          : out std_logic;                                                    -- Write Data Ready (Slave asserts)
+
+        -- Write Response Channel (B) - Slave to Master --------------------------
+        S_AXI_BRESP           : out std_logic_vector(1 downto 0);                                 -- Write Response (OKAY, SLVERR, etc.)
+        S_AXI_BVALID          : out std_logic;                                                    -- Write Response Valid (Slave asserts)
+        S_AXI_BREADY          : in  std_logic;                                                    -- Response Ready (Master asserts)
+
+        -- Read Address Channel (AR) - Master to Slave ---------------------------
+        S_AXI_ARADDR          : in  std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);              -- Read Address
+        S_AXI_ARPROT          : in  std_logic_vector(2 downto 0);                                 -- Protection Type
+        S_AXI_ARVALID         : in  std_logic;                                                    -- Read Address Valid (Master asserts)
+        S_AXI_ARREADY         : out std_logic;                                                    -- Read Address Ready (Slave asserts)
+
+        -- Read Data Channel (R) - Slave to Master -------------------------------
+        S_AXI_RDATA           : out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);              -- Read Data
+        S_AXI_RRESP           : out std_logic_vector(1 downto 0);                                 -- Read Response (OKAY, SLVERR, etc.)
+        S_AXI_RVALID          : out std_logic;                                                    -- Read Data Valid (Slave asserts)
+        S_AXI_RREADY          : in  std_logic   ); -- Read ready. This signal indicates that the master can accept the read data and response information.
 end sys_counter;
 
 architecture Behavioral of sys_counter is
